42bdcc7ec027688dec3b56931d6d741d291906b6
0219kj <jk327950@gmail.com>
2023-09-03 16:09:00 -0700

    testing the functionality

--- gt.sh
+++ gt.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#Read the commit message
-read message
+#trying to implement the usage of commandline args
 #add changes
 git add .
 #commit changes with a custom message
-git commit -m "$message"
+git commit -m "$1"
 #push changes to the remote server
 git push
